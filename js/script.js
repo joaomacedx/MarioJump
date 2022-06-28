915e171a8fb6c84cf02e9581892034e4f1b81a06
João Macêdo <99701121+joaomacedx@users.noreply.github.com>
2022-06-28 19:39:58 -0300

    Refatorando código

--- js/script.js
+++ js/script.js
@@ -2,30 +2,30 @@
  const pipe = document.querySelector('.pipe');
 
 
- const jump = () => {
-     mario.classList.add('jump');
-     setTimeout(()=>{
-      mario.classList.remove('jump');
-     },500)
- }
+ function jump() {
+    mario.classList.add('jump');
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
 
- const loop = setInterval(()=> {
+ const loop = setInterval(function () {
      const pipePosition = pipe.offsetLeft;
      const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
-          if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
-             pipe.style.animation = 'none';
-             pipe.style.left = `${pipePosition}px`;
+     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+         pipe.style.animation = 'none';
+         pipe.style.left = `${pipePosition}px`;
 
-             mario.style.animation = 'none';
-             mario.style.bottom = `${pipePosition}px`;
+         mario.style.animation = 'none';
+         mario.style.bottom = `${pipePosition}px`;
 
-             mario.src='images/game-over.png';
-             mario.style.width = '75px';
-             mario.style.marginLeft='50px';
+         mario.src = 'images/game-over.png';
+         mario.style.width = '75px';
+         mario.style.marginLeft = '50px';
 
-             clearInterval(loop);
-         }
+         clearInterval(loop);
+     }
  },10);
 
  document.addEventListener('keydown',jump);
